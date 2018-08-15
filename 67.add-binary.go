2a6c15e13ed94cf814130597d369a28996e808e0
huyang2229 <254662830@qq.com>
2018-08-15 08:26:19 +0800

    problem 67:Add Binary

--- 67.add-binary.go
+++ 67.add-binary.go
@@ -1,7 +1,8 @@
 func addBinary(a string, b string) string {
 	aLen := len(a)
 	bLen := len(b)
-	ret := []byte{}
+	ret := ""
+	tmp := ""
 	// 是否进位标志
 	in := false
 	if aLen > bLen {
@@ -9,9 +10,11 @@ func addBinary(a string, b string) string {
 		j := bLen
 		for i > 0 {
 			if j > 0 {
-				ret[i], in = calculateRule(string(a[i]), string(b[j]), in)
+				tmp, in = calculateRule(string(a[i]), string(b[j]), in)
+				ret = ret + tmp
 			} else {
-				ret[i], in = calculateRule(string(a[i]), string('0'), in)
+				tmp, in = calculateRule(string(a[i]), "0", in)
+				ret = ret + tmp
 			}
 			i--
 			j--
@@ -21,30 +24,32 @@ func addBinary(a string, b string) string {
 		j := aLen
 		for i > 0 {
 			if j > 0 {
-				ret[i], in = calculateRule(string(b[i]), string(a[j]), in)
+				tmp, in = calculateRule(string(b[i]), string(a[j]), in)
+				ret = ret + tmp
 			} else {
-				ret[i], in = calculateRule(string(b[i]), string('0'), in)
+				tmp, in = calculateRule(string(b[i]), "0", in)
+				ret = ret + tmp
 			}
 			i--
 			j--
 		}
 	}
 	if in {
-		ret = append([]byte{1}, ret...)
+		ret = "1" + ret
 	}
-	return string(ret)
+	return ret
 }
 
-func calculateRule(a string, b string, in bool) (byte, bool) {
+func calculateRule(a string, b string, in bool) (string, bool) {
 	if a == "1" && b == "1" && in {
-		return byte('1'), true
+		return "1", true
 	} else if a == "1" && b == "1" && !in {
-		return byte('0'), true
+		return "0", true
 	} else if (a == "1" && b == "0" && !in) || (a == "0" && b == "1" && !in) {
-		return byte('1'), false
+		return "1", false
 	} else if (a == "1" && b == "0" && in) || (a == "0" && b == "1" && in) {
-		return byte('0'), true
+		return "0", true
 	} else {
-		return byte('0'), false
+		return "0", false
 	}
 }
