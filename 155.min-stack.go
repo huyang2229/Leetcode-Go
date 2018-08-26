1187d422099619cabf8d501a628ab80f5b31509f
huyang2229 <254662830@qq.com>
2018-08-26 16:06:04 +0800

    problem 155:Min Stack

---
+++ 155.min-stack.go
@@ -0,0 +1,82 @@
+/*
+ * [155] Min Stack
+ *
+ * https://leetcode.com/problems/min-stack/description/
+ *
+ * algorithms
+ * Easy (32.77%)
+ * Total Accepted:    212K
+ * Total Submissions: 647K
+ * Testcase Example:  '["MinStack","push","push","push","getMin","pop","top","getMin"]\n[[],[-2],[0],[-3],[],[],[],[]]'
+ *
+ * 
+ * Design a stack that supports push, pop, top, and retrieving the minimum
+ * element in constant time.
+ * 
+ * 
+ * push(x) -- Push element x onto stack.
+ * 
+ * 
+ * pop() -- Removes the element on top of the stack.
+ * 
+ * 
+ * top() -- Get the top element.
+ * 
+ * 
+ * getMin() -- Retrieve the minimum element in the stack.
+ * 
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> Returns -3.
+ * minStack.pop();
+ * minStack.top();      --> Returns 0.
+ * minStack.getMin();   --> Returns -2.
+ * 
+ * 
+ */
+type MinStack struct {
+    
+}
+
+
+/** initialize your data structure here. */
+func Constructor() MinStack {
+    
+}
+
+
+func (this *MinStack) Push(x int)  {
+    
+}
+
+
+func (this *MinStack) Pop()  {
+    
+}
+
+
+func (this *MinStack) Top() int {
+    
+}
+
+
+func (this *MinStack) GetMin() int {
+    
+}
+
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.GetMin();
+ */
