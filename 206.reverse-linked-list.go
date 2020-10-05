0a169a3dadcf20d3c5c9232cd45149751ecdac44
huyang2229 <254662830@qq.com>
2020-10-05 12:46:04 +0800

    problem 206: reverse linked list

---
+++ 206.reverse-linked-list.go
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=golang
+ *
+ * [206] 反转链表
+ *
+ * https://leetcode-cn.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (67.10%)
+ * Likes:    1258
+ * Dislikes: 0
+ * Total Accepted:    343.9K
+ * Total Submissions: 486.8K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 反转一个单链表。
+ *
+ * 示例:
+ *
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ *
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	prev, curr := nil, head
+	for curr != nil {
+		nextTemp := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = nextTemp
+	}
+	return prev
+}
+
+// @lc code=end
+
